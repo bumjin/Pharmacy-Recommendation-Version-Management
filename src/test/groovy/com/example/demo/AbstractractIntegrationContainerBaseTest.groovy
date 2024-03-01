43530b362e4a8352f5c50648405dc0978f1af533
Beomjin Park <bumjin@gmail.com>
2024-03-02 00:31:41 +0900

    testcontainer 통합테스트환경 구성
    testcontainer로 DB redis 환경 구성

---
+++ src/test/groovy/com/example/demo/AbstractractIntegrationContainerBaseTest.groovy
@@ -0,0 +1,27 @@
+package com.example.demo
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.MariaDBContainer
+import org.testcontainers.images.PullPolicy
+import spock.lang.Specification
+
+@SpringBootTest
+abstract class AbstractractIntegrationContainerBaseTest extends Specification {
+    static final MariaDBContainer MY_DATABASE_CONTAINER
+    static final GenericContainer MY_REDIS_CONTAINER
+
+    static {
+        MY_DATABASE_CONTAINER = new MariaDBContainer("mariadb:10")
+                .withDatabaseName("pharmacyRecommendation")
+                .withImagePullPolicy(PullPolicy.alwaysPull())// Database 지정하기
+        MY_DATABASE_CONTAINER.start()
+
+        MY_REDIS_CONTAINER = new GenericContainer<>("redis:6")
+                .withExposedPorts(6379)
+        MY_REDIS_CONTAINER.start()
+        System.setProperty("spring.redis.host", MY_REDIS_CONTAINER.getHost())
+        System.setProperty("spring.redis.port", MY_REDIS_CONTAINER.getMappedPort(6379).toString())
+
+    }
+}
