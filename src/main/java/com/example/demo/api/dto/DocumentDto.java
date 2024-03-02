be3b19f604a72cdc46e42dfe754e31252588c01b
Beomjin Park <bumjin@gmail.com>
2024-03-02 12:11:38 +0900

    카카오 주소검색 테스트코드 추가
    주소검색 테스트코드 추가

--- src/main/java/com/example/demo/api/dto/DocumentDto.java
+++ src/main/java/com/example/demo/api/dto/DocumentDto.java
@@ -12,9 +12,9 @@ public class DocumentDto {
     @JsonProperty("address_name")
     private String addressName;
 
-    @JsonProperty("x")
+    @JsonProperty("y")
     private double latitude;
 
-    @JsonProperty("y")
+    @JsonProperty("x")
     private double longitude;
 }
---
+++ src/test/groovy/com/example/demo/api/service/KakaoAddressSearchServiceTest.groovy
@@ -0,0 +1,36 @@
+package com.example.demo.api.service
+
+import com.example.demo.AbstractractIntegrationContainerBaseTest
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+class KakaoAddressSearchServiceTest extends AbstractractIntegrationContainerBaseTest {
+
+    @Autowired
+    private KakaoAddressSearchService kakaoAddressSearchService
+
+    def "address 파라메터 값이 null이면 requestAddressSearch 메소드는 null을 리턴한다."() {
+        given:
+        String address = null;
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result == null
+    }
+
+    def "주소값이 valid하다면, requestAddressSearch 메소드는 정상적으로 document를 리턴한다."() {
+        given:
+        def address = "전북 삼성동 100"
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result.documentDtoList.size() > 0
+        result.metaDto.totalCount > 0
+        result.documentDtoList.get(0).addressName != null
+
+    }
+}
