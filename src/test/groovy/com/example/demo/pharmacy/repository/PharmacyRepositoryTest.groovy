3546189581dc79e4143ffcf5c504ae2bc85a9fb8
Beomjin Park <bumjin@gmail.com>
2024-03-02 16:14:29 +0900

    생성시간 수정시간 자동화
    AuditingEntityListener 사용
    entity 변경시 createDate, modifiedDate 자동생성

--- src/test/groovy/com/example/demo/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/demo/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -5,7 +5,8 @@ import com.example.demo.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+import java.time.LocalDateTime
+
 class PharmacyRepositoryTest extends AbstractractIntegrationContainerBaseTest {
 
     @Autowired
@@ -59,5 +60,25 @@ class PharmacyRepositoryTest extends AbstractractIntegrationContainerBaseTest {
         def result =  pharmacyRepository.findAll()
         then:
         result.size() == 1
+    }
+
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreateDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
     }
 }
