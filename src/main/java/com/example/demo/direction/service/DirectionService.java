c1989d42702800143773c3ed72fa1ae5c26a9c47
Beomjin Park <bumjin@gmail.com>
2024-03-03 18:57:00 +0900

    shorten URL 개발
    base62를 이용해 shorten URL 서비스 제공

--- src/main/java/com/example/demo/direction/service/DirectionService.java
+++ src/main/java/com/example/demo/direction/service/DirectionService.java
@@ -25,11 +25,17 @@ public class DirectionService {
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
     private final KakaoCategorySearchService kakaoCategorySearchService;
+    private final Base62Service base62Service;
 
     @Transactional
     public List<Direction> saveAll(List<Direction> directionList) {
         if(CollectionUtils.isEmpty(directionList)) return Collections.emptyList();
         return directionRepository.saveAll(directionList);
+    }
+
+    public Direction findById(String encodedId) {
+        Long docodedId = base62Service.decodeDirectionId(encodedId);
+        return directionRepository.findById(docodedId).orElse(null);
     }
 
 
